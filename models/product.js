058db9c82ee51d6f40e3625c6d253feb3a99c91c
Teddy-FN <tedd.ferdy@gmail.com>
2024-04-08 13:41:31 +0700

    Refactoring Code Models

--- models/product.js
+++ models/product.js
@@ -1,15 +1,34 @@
-const prod = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "product.json"
+);
+
+const getProductFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) return cb([]);
+    return cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
-  saveProduct() {
-    prod.push(this);
+  saveProduct(cb) {
+    getProductFromFile((product) => {
+      product.push(this);
+      fs.writeFile(p, JSON.stringify(product), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return prod;
+  static fetchAll(cb) {
+    getProductFromFile(cb);
   }
 };
